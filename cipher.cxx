97a2e9afaaa73bda8d4e65becb3ba6d7cccb20b8
= <osazuwaemmanuel25@gmail.com>
2024-01-01 17:49:34 +0100

    Added support for capital letters

--- cipher.cxx
+++ cipher.cxx
@@ -6,16 +6,21 @@
 using namespace std;
 bool isAlphabet(char value, string* a);
 int main(){
-  unordered_map<char, int> letters(26);
+  cout << "A Caesarean Cipher Encryption and Decryption Programme by Osazuwa.E.O \nPositive shifts can be used to encrypt, while negative shifts can be used for decryption. \nTo quit, simply type in quit as your shift value." << endl;
+  //Unordered map to store alphabets
+  unordered_map<char, int> letters;
   string alphabets = "abcdefghijklmnopqrstuvwxyz";
   int i = 0;
   for(char alphabet : alphabets){
     letters[alphabet] = i;
+    letters[toupper(alphabet)] = i;
     i++;
   }
+  //Declaring variables for input and output
   string shift_string;
   string plainText;
-  string cipherText;
+  string cipherText = "";
+  //Taking In shift value as a string to be processed
   cout << "Shift: ";
   getline(cin, shift_string);
   shift_string.erase(remove_if(shift_string.begin(), shift_string.end(), ::isspace), shift_string.end());
@@ -23,20 +28,26 @@ int main(){
   if(shift < 0){
     shift = (shift % 26) + 26;
   }
-  cout << "PlainText: ";
+  cout << "PlainText/CipherText: ";
   getline(cin, plainText);
   for(char alphabet : plainText){
     if(isAlphabet(alphabet, &alphabets)){
-      cout << alphabets[(letters[alphabet] + shift) % 26];
+      if(alphabet == toupper(alphabet)){
+      	char v = alphabets[(letters[alphabet] + shift) % 26];
+      	cipherText += toupper(v);
+      }else{
+      	cipherText += alphabets[(letters[alphabet] + shift) % 26];
+      }
     }else{
-      cout << alphabet;
+      cipherText += alphabet;
     }
   }
+  cout << "Result: "<< cipherText << endl;
   return 0;
 }
 bool isAlphabet(char value, string* a){
   for(char alphabet : *a){
-    if(value == alphabet){
+    if(value == alphabet || value == toupper(alphabet)){
       return true; 
     }
   }
